b31ce245f721fdfcea97c66d995868b047bab5b9
rafa <rajosh80@gmail.com>
2021-08-03 23:39:20 -0500

    Get api working, need to fix format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,81 @@
 export default HomePage;
+import { useState } from "react";
+import hiddenKey from "../hiddenKey";
+const axios = require("axios").default;
 
 function HomePage() {
-  async function sendEnglish() {
-    console.log("English");
+  const [french, setFrench] = useState("");
+  const [english, setEnglish] = useState("");
+  const [spanish, setSpanish] = useState("");
+  const [italian, setItalian] = useState("");
 
-    const data = {
-      key: "",
-      ui: "es",
-      text: "yes",
-      lang: "en",
-      format: "plain",
-    };
+  function sendEnglish() {
+    var subscriptionKey = hiddenKey;
+    var endpoint = "https://api.cognitive.microsofttranslator.com";
 
-    await fetch("https://translate.yandex.net/api/v1.5/tr.json/getLangs", {
-      method: "POST",
+    // Add your location, also known as region. The default is global.
+    // This is required if using a Cognitive Services resource.
+    var location = "centralus";
+
+    axios({
+      baseURL: endpoint,
+      url: "/translate",
+      method: "post",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
+        "Ocp-Apim-Subscription-Region": location,
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(data),
+      params: {
+        "api-version": "3.0",
+        from: "en",
+        to: ["fr", "it", "es"],
+      },
+      data: [
+        {
+          text: english,
+        },
+      ],
+      responseType: "json",
     })
-      .then((res) => {
-        console.log("English sent");
-        console.log(res);
+      .then(function (response) {
+        setFrench(response.data[0].translations[0].text);
+        setItalian(response.data[0].translations[1].text);
+        setSpanish(response.data[0].translations[2].text);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log("RRRRRR", error);
       });
   }
 
   return (
-    <div style={{ backgroundColor: "black", height: 900, color: "white" }}>
+    <div style={{ backgroundColor: "black", height: 1000, color: "white" }}>
       <div>
         <h1 style={{ textAlign: "center", paddingTop: 100, paddingBottom: 60 }}>
           Welcome to Triple Translator!
         </h1>
       </div>
-      <div style={{ textAlign: "center" }}>
-        <div style={{ flex: 3, display: "inline-block" }}>
-          <h2 style={{ marginBottom: 40 }}>
-            Translate English to Spanish, French, and Italian all at once!
-          </h2>
+      <h2 style={{ margin: 40 }}>
+        Translate English to Spanish, French, and Italian all at once!
+      </h2>
 
-          <div style={{ flexDirection: "column" }}>
-            <h2 style={{ display: "inline-block", marginRight: 40 }}>
+      <div
+        style={{
+          textAlign: "center",
+          flexDirection: "row",
+          display: "inline-block",
+          width: "100%",
+        }}
+      >
+        <div style={{ flex: 1, padding: 50, display: "inline-block" }}>
+          <div style={{ flexDirection: "row" }}>
+            <h2 style={{ marginRight: 0, display: "inline-block" }}>
               English:
             </h2>
             <input
               type="text"
-              style={{ width: 400 }}
+              style={{ width: 400, display: "inline-block" }}
+              onChange={(e) => setEnglish(e.target.value)}
               onSubmit={async () => await sendEnglish()}
               onKeyPress={async (e) => {
                 if (e.key === "Enter") {
@@ -66,31 +94,30 @@ function HomePage() {
             >
               Spanish
               <div>
-                <textarea style={{ height: 400 }} />
+                <textarea style={{ height: 200 }} value={spanish} />
               </div>
             </div>
 
             <div style={{ display: "inline-block", marginRight: 40 }}>
               French
               <div>
-                <textarea style={{ height: 400 }} />
+                <textarea style={{ height: 200 }} value={french} />
               </div>
             </div>
 
             <div style={{ display: "inline-block", marginRight: 40 }}>
               Italian
               <div>
-                <textarea style={{ height: 400 }} />
+                <textarea style={{ height: 200 }} value={italian} />
               </div>
             </div>
           </div>
         </div>
         <div
           style={{
-            flex: 2,
+            flex: 1,
+            marginLeft: 0,
             display: "inline-block",
-            marginLeft: 250,
-            flexDirection: "column",
           }}
         >
           <h2 style={{}}>Read these tips!</h2>
@@ -98,8 +125,8 @@ function HomePage() {
             style={{
               borderColor: "white",
               border: "1px solid white",
-              height: 500,
-              width: 400,
+              height: 300,
+              width: 100,
             }}
           >
             {" "}
