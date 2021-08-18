ffa868495976d585c6ffe230bc9ca193f30b9651
rafie97 <rajosh80@gmail.com>
2021-08-17 19:39:18 -0500

    es trip upates

---
+++ hiddenKey.ts
@@ -0,0 +1,2 @@
+const key = "81ebfad3ebd94a6db422173530d48f06";
+export default key;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 export default HomePage;
 import { useState } from "react";
 import hiddenKey from "../hiddenKey";
+import { AiFillSound } from "react-icons/ai";
 const axios = require("axios").default;
 
 function HomePage() {
@@ -55,23 +56,30 @@ function HomePage() {
           Welcome to Triple Translator!
         </h1>
       </div>
-      <h2 style={{ margin: 40 }}>
+      <h2 style={{ margin: 40, marginLeft: 100 }}>
         Translate English to Spanish, French, and Italian all at once!
       </h2>
 
       <div
         style={{
           textAlign: "center",
-          flexDirection: "row",
-          display: "inline-block",
+          flexDirection: "column",
           width: "100%",
         }}
       >
-        <div style={{ flex: 1, padding: 50, display: "inline-block" }}>
-          <div style={{ flexDirection: "row" }}>
-            <h2 style={{ marginRight: 0, display: "inline-block" }}>
+        <div
+          style={{
+            flex: 1,
+            padding: 50,
+            display: "inline-block",
+            position: "absolute",
+            left: 100,
+          }}
+        >
+          <div style={{ flexDirection: "row", marginBottom: 30 }}>
+            <h3 style={{ marginRight: 20, display: "inline-block" }}>
               English:
-            </h2>
+            </h3>
             <input
               type="text"
               style={{ width: 400, display: "inline-block" }}
@@ -96,6 +104,11 @@ function HomePage() {
               <div>
                 <textarea style={{ height: 200 }} value={spanish} />
               </div>
+              <AiFillSound
+                style={{ marginTop: 10 }}
+                color="white"
+                size={30}
+              ></AiFillSound>
             </div>
 
             <div style={{ display: "inline-block", marginRight: 40 }}>
@@ -103,6 +116,11 @@ function HomePage() {
               <div>
                 <textarea style={{ height: 200 }} value={french} />
               </div>
+              <AiFillSound
+                style={{ marginTop: 10 }}
+                color="white"
+                size={30}
+              ></AiFillSound>
             </div>
 
             <div style={{ display: "inline-block", marginRight: 40 }}>
@@ -110,6 +128,11 @@ function HomePage() {
               <div>
                 <textarea style={{ height: 200 }} value={italian} />
               </div>
+              <AiFillSound
+                style={{ marginTop: 10 }}
+                color="white"
+                size={30}
+              ></AiFillSound>
             </div>
           </div>
         </div>
@@ -118,21 +141,48 @@ function HomePage() {
             flex: 1,
             marginLeft: 0,
             display: "inline-block",
+            position: "absolute",
+            right: 100,
           }}
         >
-          <h2 style={{}}>Read these tips!</h2>
+          <h2>Read these tips!</h2>
           <div
             style={{
               borderColor: "white",
               border: "1px solid white",
-              height: 300,
-              width: 100,
+              height: 600,
+              width: 400,
             }}
           >
-            {" "}
+            <ul>
+              <li style={styles.tipListItem}>
+                Interchangable Letters: <br></br>
+                J=I=Y <br></br>
+                C=S=Z <br></br>
+                W=V=U
+              </li>
+              <li style={styles.tipListItem}>Like in English, tion=sion</li>
+              <li style={styles.tipListItem}>
+                Often pronouns such as "I" and "he" are absorbed by the verb and
+                the prefix or suffix changes from the root verb
+              </li>
+              <li style={styles.tipListItem}>
+                Sometimes translations sound like a word in English but are a
+                rare synonym of the original. Think about what English word the
+                translation sounds like and try to build an intuition for how
+                it's similar.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const styles = {
+  tipListItem: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
